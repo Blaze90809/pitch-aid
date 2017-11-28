e1f448bd90f9b1244d321f556ff8e3643b9976b1
Blaze Fugina <blaze.fugina@gmail.com>
2017-11-28 16:35:53 -0600

    Base project completed.

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('UserController', function(UserService) {
+myApp.controller('UserController', function(UserService, $mdToast) {
   console.log('UserController created');
   var vm = this;
   vm.userService = UserService;
@@ -9,7 +9,17 @@ myApp.controller('UserController', function(UserService) {
   vm.addPitchers = (pitcher) => {
     UserService.addPitchers(pitcher);
     console.log('Pitcher Added');
-    vm.pitcher = '';
+    vm.pitcher = null;
   }
+  
+  vm.showToast = () => {
+    console.log('Toast added')
+    $mdToast.show(
+      $mdToast.simple()
+      .textContent('Pitcher Added!')
+      .hideDelay(3000)
+    )
+  }
+
 
 });
